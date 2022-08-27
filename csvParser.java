6ab4b0972cecf72775f5655be5dc1e80687276df
David Mitchell <davidbmitchell77@outlook.com>
2022-08-27 16:30:00 -0700

    Update csvParser.java

--- csvParser.java
+++ csvParser.java
@@ -4,16 +4,16 @@
 /* @author        - David B. Mitchell                                          */
 /* @description   - Anonymous Apex class for parsing csv data.                 */
 /*-----------------------------------------------------------------------------*/
-
 String body = '';
 
-String csv = 'Id,Name,Phone,Address,State,PostalCode';
+String csv = 'Id,Name,Phone,Address,City,State,PostalCode';
 csv += '\n';
 
 csv += ''  + '0000000001';
-csv += ',' + '"Blue Oasis Technologies, Inc."';
+csv += ',' + '"Blue Oasis Technologies,, Inc."';
 csv += ',' + '(619) 216-1001';
 csv += ',' + '380 Fourth Street';
+csv += ',' + 'Downey';
 csv += ',' + 'CA';
 csv += ',' + '90660';
 csv += '\n';
@@ -22,6 +22,7 @@ csv += ''  + '0000000002';
 csv += ',' + '"Alfred ""Eddy"" Neuman"';
 csv += ',' + '(888) 562-5442';
 csv += ',' + '88 Citrus Way';
+csv += ',' + 'Jacksonville';
 csv += ',' + 'FL';
 csv += ',' + '20110';
 csv += '\n';
@@ -30,6 +31,7 @@ csv += ''  + '0000000003';
 csv += ',' + 'Gene Simmons';
 csv += ',' + '(213) 282-2882';
 csv += ',' + '1 Rodeo Dr';
+csv += ',' + 'Beverly Hills';
 csv += ',' + 'CA';
 csv += ',' + '90210';
 csv += '\n';
@@ -38,6 +40,7 @@ csv += ''  + '0000000004';
 csv += ',' + '"Paul ""Ace"" Frehley"';
 csv += ',' + '(212) 222-5978';
 csv += ',' + '88 Citrus Way';
+csv += ',' + 'Bronx';
 csv += ',' + 'NY';
 csv += ',' + '10468';
 csv += '\n';
@@ -45,18 +48,18 @@ csv += '\n';
 for (String row : csv.split('\n'))
 {
     if (!row.replace('""', '&quot;').contains('"')) {
-      body += row + '\n';
+      body += row + '\n';    
     }
     else
     {
         String s = row.replace('""', '&quot;');
         String[] values = s.split(',');
-        for (Integer i=0; i<values.size(); i++)
+        for (String value : values)
         {
-            if (values[i] > '')
+            if (value > '')
             {
-                String buffer = values[i];
-                if (values[i].startsWith('"') && !values[i].contains('&quot;')) { buffer +=  '&comma;'; }
+                String buffer = value;
+                if (value.startsWith('"') && (!value.contains('&quot;'))) { buffer +=  '&comma;'; }
                 if (!buffer.endsWith('&comma;')) { buffer += ','; }
                 body += buffer;
             }
@@ -67,5 +70,4 @@ for (String row : csv.split('\n'))
 
 body = body.replace('&quot;', '""');
 body = body.removeEnd('\n');
-
 System.debug(LoggingLevel.INFO, 'body:\n' + body);
